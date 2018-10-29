8725a09e43447509783d4579b3ab7a12279fea5c
Adriana Lucia Caipa Furque <UANDES\al.caipa10@fisicaez12210l.ad.uniandes.edu.co>
2018-10-29 17:03:01 -0500

    Comparacion de Interpolacion

--- Fourier.py
+++ Fourier.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft, ifft
+from scipy import interpolate 
 
 senal= np.genfromtxt('signal.dat', delimiter = ',')
 incompletos= np.genfromtxt('incompletos.dat',delimiter=',')
@@ -33,5 +34,48 @@ inversa= np.fft.ifft(valores1)
 plt.figure()
 plt.plot(senal[:,0],inversa)
 plt.savefig('CaipaAdriana_filtrada.pdf')
+
+print "No se puede realizar la transformada de Fourier de los datos incompletos porque ..."
+
+x_tabla= incompletos[:,0]
+y_tabla= incompletos[:,1]
+x1=np.linspace(0.000390625,0.028515625,512)
+###Interpolacion de datos incompletos###
+def interpolacion_cuad(x1,x_tabla,y_tabla):
+    funcion_cuadratica=interpolate.interp1d(x_tabla,y_tabla,kind = "quadratic")
+    return (funcion_cuadratica(x1))
+def interpolacion_cub(x1,x_tabla,y_tabla):
+    funcion_cubica=interpolate.interp1d(x_tabla,y_tabla,kind= "cubic")
+    return (funcion_cubica(x1))
+
+dt=x1[2]-x1[1]
+fourier1= np.fft.fft(interpolacion_cuad(x1,x_tabla,y_tabla))
+fourier2= np.fft.fft(interpolacion_cub(x1,x_tabla,y_tabla))
+
+frecuencias1=np.fft.fftfreq(x1.shape[0],dt)
+frecuencias2=np.fft.fftfreq(x1.shape[0],dt)
+#######Grafica fourier###
+plt.figure()
+plt.subplot(311)
+plt.plot(frecuencias, fourier,c='m', label = 'Fourier senal')
+plt.legend()
+plt.subplot(312)
+plt.plot(frecuencias1, fourier1,c='c', label = 'Interpola cuad')
+plt.legend()
+plt.subplot(313)
+plt.plot(frecuencias2, fourier2,c='r', label = 'Interpola cub')
+plt.legend()
+plt.savefig("CaipaAdriana_TF_Interpola.pdf")
+
+#####Filtro####
+print "Los valores mas alejados del cero cambian mucho la amplitud, comparando la interpolacion cuadratica, cubica y la senal"
+
+valores_2=np.zeros(frecuencias1.shape[0])
+for i in range(frecuencias1.shape[0]):
+	if abs(frecuencias1[i])<=1000:
+		valores_2[i]=1
+valores_nuevos= fourier*valores_2
+inversa1= np.fft.ifft(valores_nuevos)
+####Corte 500Hz###
 
 
