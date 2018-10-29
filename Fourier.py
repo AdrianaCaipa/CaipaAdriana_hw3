3bd4c56b88e330bd00a85af1f5e6185623b234f0
Adriana Lucia Caipa Furque <UANDES\al.caipa10@fisicaez12210l.ad.uniandes.edu.co>
2018-10-29 14:54:55 -0500

    inversa

--- Fourier.py
+++ Fourier.py
@@ -10,15 +10,26 @@ fourier=np.zeros(senal.shape[0])
 arreglo=np.arange(senal.shape[0])
 for k in range (senal.shape[0]):
 	fourier[k]=np.sum(senal[:,1]*np.exp((-2*1j*np.pi*k*(arreglo)/senal.shape[0])))
-dt= senal[1,0]-senal[2,0]
+dt= senal[2,0]-senal[1,0]
 print dt
 frecuencias=np.fft.fftfreq(senal.shape[0],dt)
-	
+gfkdg = np.fft.fft(senal[:,1])
 plt.figure()
-plt.plot(frecuencias,fourier)
-plt.show()
-#plt.savefig('CaipaAdriana_TF.pdf')
+plt.plot(frecuencias,abs(fourier))
+
+plt.savefig('CaipaAdriana_TF.pdf')
 plt.figure()
 plt.plot(senal[:,0],senal[:,1])
 plt.savefig("CaipaAdriana_Signal.pdf")
 print "Mayores frecuencias en 383.3, 237,138.5"
+
+valores=np.zeros(frecuencias.shape[0])
+for i in range(frecuencias.shape[0]):
+	if abs(frecuencias[i])<=1000:
+		valores[i]=1
+
+valores1= fourier*valores
+inversa= np.fft.ifft(valores1)
+plt.figure()
+plt.plot(senal[:,0],inversa)
+plt.show()
