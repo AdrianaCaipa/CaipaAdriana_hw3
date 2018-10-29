6e2a330b190623d7558278bbaf73c212543ee225
Adriana Lucia Caipa Furque <UANDES\al.caipa10@fisicaez12210l.ad.uniandes.edu.co>
2018-10-29 13:36:18 -0500

    Trasnformada de Fourier

---
+++ Fourier.py
@@ -0,0 +1,22 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+from scipy.fftpack import fft, ifft
+
+senal= np.genfromtxt('signal.dat', delimiter = ',')
+incompletos= np.genfromtxt('incompletos.dat',delimiter=',')
+
+fourier=np.zeros(senal.shape[0])
+
+arreglo=np.arange(senal.shape[0])
+for k in range (senal.shape[0]):
+	fourier[k]=np.sum(senal[:,1]*np.exp((-2*1j*np.pi*k*(arreglo)/senal.shape[0])))
+dt= senal[1,0]-senal[2,0]
+print dt
+frecuencias=np.fft.fftfreq(senal.shape[0],dt)
+	
+plt.figure()
+plt.plot(frecuencias,fourier)
+plt.savefig('CaipaAdriana_TF.pdf')
+
+
+
