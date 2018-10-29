60bcf9f24a96aa04fa02c7957237fb3e40a4947b
Adriana Lucia Caipa Furque <UANDES\al.caipa10@fisicaez12207l.ad.uniandes.edu.co>
2018-10-29 11:53:51 -0500

    Plotear grafica

--- PCR.py
+++ PCR.py
@@ -20,7 +20,7 @@ for i in range(datos_nuevos.shape[1]):
 matriz_cov=np.zeros([datos_nuevos.shape[1],datos_nuevos.shape[1]])	
 for i in range(datos_nuevos.shape[1]):
 	for j in range(datos_nuevos.shape[1]):
-		matriz_cov[i,j]= np.sum(x[:,i]*x[:,j])/(datos_nuevos.shape[1]-1)
+		matriz_cov[i,j]= np.sum(x[:,i]*x[:,j])/(datos_nuevos.shape[0]-1)
 		
 print matriz_cov
 
@@ -31,4 +31,16 @@ for i in range(len(w)):
 	print "El valor propio es",w[i]
 	print "El vector propio es",v[i]  
 
-###
+print "Los parametros mas importantes en base a los componentes son los 2 primeros"
+
+####Grafica###v[0,:]
+ejex= np.dot(x,v[0,:])
+ejey=np.dot(x,v[1,:])
+print ejex
+print ejey
+plt.figure()
+plt.scatter(ejex[np.where(etiqueta[:,1]=='B')],ejey[np.where(etiqueta[:,1]=='B')],c='m', label = 'Benigno')
+plt.scatter(ejex[np.where(etiqueta[:,1]=='M')],ejey[np.where(etiqueta[:,1]=='M')],c='c', label = 'Maligno')
+plt.legend()
+plt.savefig('CaipaAdrianaPCA.pdf')
+
